40a2b94db2baf4426ae0ceb29790f2830cf0141c
Vincent Hould <vinc042@gmail.com>
2022-02-03 21:42:06 -0500

    Added dash action

--- Assets/Scripts/Player/Controllable.cs
+++ Assets/Scripts/Player/Controllable.cs
@@ -2,14 +2,17 @@
 
 public class Controllable : MonoBehaviour
 {
+    [SerializeField] private bool isEnabled = true;
+    [SerializeField] private bool isLocked = false;
     [SerializeField] private float speed = 10f;
+    [SerializeField] private float speedMultiplier = 1f;
 
     private Animator animator;
     private SpriteRenderer spriteRenderer;
 
     private float move;
     private float spriteWidth;
-    private bool isEnabled = true;
+
 
     private void Start()
     {
@@ -24,15 +27,23 @@ public class Controllable : MonoBehaviour
     {
         if (isEnabled)
         {
-            move = Input.GetAxisRaw("Horizontal");
-            if (move != 0f)
+            if (!isLocked)
             {
-                animator.SetBool("isRunning", true);
-                spriteRenderer.flipX = move < 0f;
-            }
-            else
-            {
-                animator.SetBool("isRunning", false);
+                move = Input.GetAxisRaw("Horizontal");
+                if (move != 0f)
+                {
+                    animator.SetBool("isRunning", true);
+                    spriteRenderer.flipX = move < 0f;
+                }
+                else
+                {
+                    animator.SetBool("isRunning", false);
+                }
+
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    animator.SetTrigger("dash");
+                }
             }
         } else
         {
@@ -45,12 +56,17 @@ public class Controllable : MonoBehaviour
         Camera cam = Camera.main;
         float minX = cam.ScreenToWorldPoint(new Vector3(0, 0, 0)).x + (spriteWidth / 2);
         float maxX = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x - (spriteWidth / 2);
-        float newX = Mathf.Clamp(transform.position.x + (move * speed * Time.fixedDeltaTime), minX, maxX);
+        float newX = Mathf.Clamp(transform.position.x + (move * speed * speedMultiplier * Time.fixedDeltaTime), minX, maxX);
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
 
     private void Disable()
     {
         isEnabled = false;
+    }
+
+    private void SetLocked(int locked)
+    {
+        isLocked = locked > 0;
     }
 }
